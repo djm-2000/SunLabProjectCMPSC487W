949409ec2daed5f4cc47e5cc657e6ccc971a7d8b
dean <djm7427@psu.edu>
2022-09-15 00:01:17 -0400

    Updated App.java, DatabaseAccessObject.java

--- DatabaseAccessObject.java
+++ DatabaseAccessObject.java
@@ -1,99 +1,58 @@
-package com.example.insurancedatabase;
+package com.example.sunlab;
 
 import java.sql.*;
-
 import oracle.jdbc.pool.OracleDataSource;
 
 import java.util.Scanner;
 
-public class DatabaseAccessObject {
+public class DatabaseAccessObject
+{
     static Scanner scn = new Scanner(System.in);
-    static String username, password;
-
-    // TODO: refine data types for table attributes
-    public static void initializeDatabase(Statement s) {
-        String dropCustomerTable = "drop table Customer";
-        String dropCarInsuranceTable = "drop table car_insurance";
-        String dropHomeInsuranceTable = "drop table home_insurance";
-        String dropLifeInsuranceTable = "drop table life_insurance";
-
-        String createCustomerTable = "create table customer ( "
-                + "customer_id numeric (14, 0), "
-                + "first_name varchar (20), "
-                + "last_name varchar (20), "
-                + "primary key (customer_id) "
+    public static void initializeDatabase(Statement s)
+    {
+        String dropTable = "drop table student_login";
+        String createTable = "create table student_login ( "
+                + "student_id varchar (9), "
+                + "timestamp varchar (26),"
+                + "primary key (timestamp) "
                 + " )";
-
-        String createCarInsuranceTable = "create table car_insurance ( "
-                + "car_insurance_id numeric (14, 0), "
-                + "make varchar (20), "
-                + "year numeric (4, 0), "
-                + "VIN varchar (30), "
-                + "mileage_per_year varchar (30), "
-                + "coverage varchar (30), "
-                + "monthly_payment numeric (10, 2), "
-                + "start_date varchar (10), "
-                + "end_date varchar (10), "
-                + "customer_id numeric (14, 0), "
-                + "primary key (car_insurance_id), "
-                + "foreign key (customer_id) references customer)";
-
-        String createHomeInsuranceTable = "create table home_insurance ( "
-                + "home_insurance_id numeric (14, 0), "
-                + "house_address varchar (40), "
-                + "house_area numeric (5, 0), "
-                + "num_bedrooms numeric (2, 0), "
-                + "num_bathrooms numeric (2, 0), "
-                + "house_price numeric (10, 2), "
-                + "coverage varchar (40), "
-                + "monthly_payment numeric (10, 2), "
-                + "start_date varchar (10), "
-                + "end_date varchar (10), "
-                + "customer_id numeric (14, 0), "
-                + "primary key (home_insurance_id), "
-                + "foreign key (customer_id) references customer)";
-
-        String createLifeInsuranceTable = "create table life_insurance ( "
-                + "life_insurance_id numeric (14, 0), "
-                + "existing_conditions varchar (200), "
-                + "coverage varchar (100), "
-                + "beneficiary varchar (100), "
-                + "monthly_payment numeric (10, 2), "
-                + "start_date varchar (10), "
-                + "customer_id numeric (14, 0), "
-                + "primary key (life_insurance_id), "
-                + "foreign key (customer_id) references customer)";
-
-        try {
-            s.execute(createCustomerTable);
-            s.execute(createCarInsuranceTable);
-            s.execute(createHomeInsuranceTable);
-            s.execute(createLifeInsuranceTable);
-        } catch (SQLException e) {
+        try
+        {
+            s.execute(dropTable);
+            s.execute(createTable);
+        }
+        catch (SQLException e)
+        {
             e.printStackTrace();
         }
 
 
     }
-
-    public static Connection getDatabaseConnection(String username, String password) {
+    public static Connection getDatabaseConnection(String username, String password)
+    {
         Connection connection = null;
         OracleDataSource ods = null;
-        try {
+        try
+        {
             ods = new OracleDataSource();
 
-            ods.setServiceName("orclpdb.hbg.psu.edu");
-            ods.setURL("jdbc:oracle:thin:@h3oracle.ad.psu.edu:1521/orclpdb.ad.psu.edu");
-        } catch (SQLException e) {
+            ods.setServiceName("orcl");
+            ods.setURL("jdbc:oracle:thin:@localhost:1521");
+        }
+        catch (SQLException e)
+        {
             e.printStackTrace();
         }
 
         ods.setUser(username);
         ods.setPassword(password);
 
-        try {
+        try
+        {
             connection = ods.getConnection();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
 
         }
 
@@ -101,147 +60,128 @@ public class DatabaseAccessObject {
 
     }
 
-    public static String[] getPolicies(Statement s) {
-        int numCarPolicies = getNumberOfCarPolicies(s);
-        int numHomePolicies = getNumberOfHomePolicies(s);
-        int numLifePolicies = getNumberOfLifePolicies(s);
 
-        String[] policyInfo = new String[numCarPolicies + numHomePolicies + numLifePolicies];
 
-        String selectCar = "select * from car_insurance";
-        String selectHome = "select * from home_insurance";
-        String selectLife = "select * from life_insurance";
 
+    public static void addLogin(Statement s, String studentID, Timestamp timestamp)
+    {
+        PreparedStatement ps;
+        int id = getNumberOfLogins(s) + 1;
+        //  String query = "insert into student_login values (" + studentID + ", " + timestamp.toString().split(" ")[1] +  ")";
+        String query = "insert into student_login values (?,?)";
+        // TODO: don't hardcode username and password
+        try {
+            ps = getDatabaseConnection("system", "oracle").prepareStatement(query);
+            ps.setString(1, studentID);
+            ps.setString(2, timestamp.toString());
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try
+        {
+            ps.execute();
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static String[] getLogins(Statement s) {
+
+        int numberOfLogins = getNumberOfLogins(s);
+
+        String[] loginInfo = new String[numberOfLogins];
+
+        String query = "select * from student_login";
         ResultSet resultSet = null;
         try {
-            // add car policies
-            resultSet = s.executeQuery(selectCar);
+            resultSet = s.executeQuery(query);
 
-            for (int i = 0; i < numCarPolicies; i++) {
+            for (int i = 0; i < numberOfLogins; i++) {
                 resultSet.next();
-
+                loginInfo[i] = resultSet.getString("student_id")
+                        + " " + resultSet.getString("timestamp");
             }
-
-
-        } catch (SQLException e) {
-
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
         }
 
-        return policyInfo;
+        return loginInfo;
     }
 
-    public static String[] getCustomers(Statement s) {
-        int numberOfCustomers = getNumberOfCustomers(s);
-        String[] customerInfo = new String[numberOfCustomers];
+    public static String[] getLoginsByID(Statement s) {
 
-        String query = "select * from Customer";
+        int numberOfLogins = getNumberOfLogins(s);
+
+        String[] loginInfo = new String[numberOfLogins];
+
+        String query = "select * from student_login ORDER BY student_id ASC";
         ResultSet resultSet = null;
         try {
             resultSet = s.executeQuery(query);
 
-            for (int i = 0; i < numberOfCustomers; i++) {
+            for (int i = 0; i < numberOfLogins; i++) {
                 resultSet.next();
-                customerInfo[i] = resultSet.getString("first_name")
-                        + " " + resultSet.getString("last_name");
+                loginInfo[i] = resultSet.getString("student_id")
+                        + " " + resultSet.getString("timestamp");
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e)
+        {
             e.printStackTrace();
         }
 
-        return customerInfo;
+        return loginInfo;
     }
 
-    public static boolean customerExists(Statement s, String firstName, String lastName) {
+    public static String[] getLoginsByTime(Statement s) {
 
-        String query = "select customer_id from customer where first_name = " + "\'" + firstName + "\'" + " and last_name = "
-                + "\'" + lastName + "\'";
-        ResultSet resultSet;
-        try {
-            resultSet = s.executeQuery(query);
-            if (resultSet.next())
-                return true;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
+        int numberOfLogins = getNumberOfLogins(s);
 
+        String[] loginInfo = new String[numberOfLogins];
 
-    public static void addCustomer(Statement s, String firstName, String lastName) {
-        int id = getNumberOfCustomers(s) + 1;
-        String query = "insert into Customer values (" + id + ", \'" + firstName + "\'" + "," + "\'" + lastName + "\'" + ")";
-
-        try {
-            s.execute(query);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public static int getNumberOfCustomers(Statement s) {
-        int numberOfCustomers = -1;
-        String query = "select count(customer_id) from Customer";
+        String query = "select * from student_login ORDER BY timestamp ASC";
         ResultSet resultSet = null;
         try {
             resultSet = s.executeQuery(query);
 
-            resultSet.next();
-            numberOfCustomers = resultSet.getInt(1);
-        } catch (SQLException e) {
+            for (int i = 0; i < numberOfLogins; i++) {
+                resultSet.next();
+                loginInfo[i] = resultSet.getString("student_id")
+                        + " " + resultSet.getString("timestamp");
+            }
+        }
+        catch (SQLException e)
+        {
             e.printStackTrace();
         }
 
-        return numberOfCustomers;
+        return loginInfo;
     }
 
-    public static int getNumberOfLifePolicies(Statement s) {
-        int numLifePolicies = -1;
-        String query = "select count(life_insurance_id) from life_insurance";
-        ResultSet resultSet = null;
+    public static int getNumberOfLogins(Statement s) {
+
+        int numberOfLogins = -1;
+        String query = "select count(student_id) from student_login";
+
+        ResultSet rs = null;
+
         try {
-            resultSet = s.executeQuery(query);
+            rs = s.executeQuery(query);
 
-            resultSet.next();
-            numLifePolicies = resultSet.getInt(1);
-        } catch (SQLException e) {
+            rs.next();
+            numberOfLogins = rs.getInt(1);
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
 
-        return numLifePolicies;
+        return numberOfLogins;
     }
-
-    public static int getNumberOfHomePolicies(Statement s) {
-        int numHomePolicies = -1;
-        String query = "select count(home_insurance_id) from home_insurance";
-        ResultSet resultSet = null;
-        try {
-            resultSet = s.executeQuery(query);
-
-            resultSet.next();
-            numHomePolicies = resultSet.getInt(1);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return numHomePolicies;
-    }
-
-    public static int getNumberOfCarPolicies(Statement s) {
-        int numCarPolicies = -1;
-        String query = "select count(car_insurance_id) from car_insurance";
-        ResultSet resultSet = null;
-        try {
-            resultSet = s.executeQuery(query);
-
-            resultSet.next();
-            numCarPolicies = resultSet.getInt(1);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return numCarPolicies;
-    }
-
 
 }
